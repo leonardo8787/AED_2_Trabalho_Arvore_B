cc444cf3424cf04178c10ee608365923a363a882
leonardo8787 <leocampos8787@gmail.com>
2021-12-19 12:52:22 -0300

    atualização

---
+++ src/main.c
@@ -0,0 +1,36 @@
+#include "b_tree.h"
+
+int main(){
+
+	int reg1[] = {10,2,7,5,3,9,16,4,11,1,6,23,14};
+	int reg2[] = {10,2,7,5,3,9,16,4,11,1,6,23,14};
+	
+	Record r;
+  	Pagina *btree = CreateBTree();
+  	int ordem = 1;
+
+  	printf("\nInserção\n");
+  	for(int i=0; i<13; i++){
+    	r.key = reg1[i];
+    	Insere(&btree, r);
+    	printf("ok");
+    	printf("\n");
+  	}
+  	
+  	printf("\nImpressão\n");
+  	Imprime(&btree, 0);
+
+	printf("\nRemoção\n");
+
+  	for(int i=0; i<13; i++){
+  		Remove(&btree, reg2[i], &ordem);
+  		printf("ok");
+  		printf("\n");
+  	}
+
+  	printf("\nImpressão\n");
+  	Imprime(&btree, 0);
+	
+	return 0;
+
+}
